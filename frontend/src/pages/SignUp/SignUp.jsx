4a9bfd4f3a4d404a7b25b6b517ac1f87a0333871
RajatSharma <withrajatsharma@gmail.com>
2024-06-08 18:46:23 +0530

    .

---
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SignUp = () => {
+
+    
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Login</h4>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
