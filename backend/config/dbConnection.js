56c80142ac01fef508f4c2109e01d64572a052f0
RajatSharma <withrajatsharma@gmail.com>
2024-06-12 22:52:10 +0530

    Create dbConnection.js

---
+++ backend/config/dbConnection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbConnection = () => {
+    mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => {
+        console.log(`database connected successfully`);
+    })
+    .catch((err) => {
+        console.log(`error occured while connecting database`);
+        process.exit(1);
+    })
+
+}
+
+module.exports = dbConnection;
+
