da049d77979656f890b58ff53deef74f2815924c
RajatSharma <withrajatsharma@gmail.com>
2024-06-13 22:22:59 +0530

    Create utilities.js

---
+++ backend/utilities.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+function  authenticateToken(req, res, next) {
+
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.sendStatus(401);
+
+
+  try {
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decodedToken;
+    next();
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message:" error while authorizing token",
+      error:error.message
+    });
+    
+  }
+
+  // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  //   if (err) return res.sendStatus(401);
+  //   req.user = user;
+  //   next();
+  // });
+}
+
+
+module.exports = {
+  authenticateToken,
+};
