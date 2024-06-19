9cf8678988c969e4c37e2cde6fddf137b99a2b35
RajatSharma <withrajatsharma@gmail.com>
2024-06-19 17:14:45 +0530

    Create axiosInstance.js

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    withCredentials: true,
+    baseURL: 'http://localhost:5000',
+    timeout:10000,
+    headers:{
+        "Content-Type":"application/json",
+    },
+});
+
+// axiosInstance.interceptors.request.use(
+//     (config) =>{
+//         const accessToken = localStorage.getItem('token');
+//         if(accessToken) {
+//             config.headers.Authorization = `Bearer ${accessToken}`;
+//             };
+//         return config;
+//     },
+//     (error)=>{
+//         return Promise.reject(error);
+//     }
+
+    
+// )
+
+export default axiosInstance;
