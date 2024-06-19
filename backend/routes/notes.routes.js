6b0737d845db76e6bc4f05c4825aa8f407eb57fc
RajatSharma <withrajatsharma@gmail.com>
2024-06-19 17:14:28 +0530

    Update notes.routes.js

--- backend/routes/notes.routes.js
+++ backend/routes/notes.routes.js
@@ -1,16 +1,15 @@
 
 const express = require('express');
 const router = express.Router();
-const {authenticateToken} = require('../utilities')
-
 const {addNote,editNote, getAllNotes, deleteNote,updatePinned, searchNote} = require("../controllers/notes.controller");
+const {auth}  = require('../middlewares/auth.js');
 
-router.post("/add-note",authenticateToken,addNote);
-router.put("/edit-note/:noteId",authenticateToken,editNote);
-router.get("/get-all-notes",authenticateToken,getAllNotes);
-router.delete("/delete-note/:noteId",authenticateToken,deleteNote);
-router.put("/update-pinned/:noteId",authenticateToken,updatePinned);
-router.get("/search-note",authenticateToken,searchNote);
+router.post("/add-note",auth,addNote);
+router.put("/edit-note/:noteId",auth,editNote);
+router.get("/get-all-notes",auth,getAllNotes);
+router.delete("/delete-note/:noteId",auth,deleteNote);
+router.put("/update-pinned/:noteId",auth,updatePinned);
+router.get("/search-note",auth,searchNote);
 
 
 
