383faea0545f49905c567886d9a27f348a56c3c7
RajatSharma <withrajatsharma@gmail.com>
2024-06-19 17:14:39 +0530

    Create UserContext.jsx

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,40 @@
+import axiosInstance from "../utils/axiosInstance";
+import { createContext, useEffect, useState } from "react";
+
+export const UserContext = createContext();
+
+const UserContextProvider = ({children}) => {
+
+    const [userInfo, setUserInfo] = useState(null);
+
+    const getUserInfo = async () => {
+        try {
+          const response = await axiosInstance.get("/auth/get-user");
+    
+          if (response.data.user) {
+            
+            setUserInfo(response.data.user);
+            
+          }
+        } catch (error) {
+          if(!error.response.data.success){
+            console.log(`error: ${error.response.data.message}`);
+          }
+        }
+      };
+
+
+      useEffect(()=>{
+        getUserInfo();
+      },[])
+
+
+
+    return (
+        <UserContext.Provider value={{userInfo, setUserInfo}} >
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
