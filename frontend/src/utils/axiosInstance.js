40930a725906453b8694fc79c249f075f11be58e
RajatSharma <withrajatsharma@gmail.com>
2024-06-20 21:15:29 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -10,19 +10,5 @@ const axiosInstance = axios.create({
     },
 });
 
-// axiosInstance.interceptors.request.use(
-//     (config) =>{
-//         const accessToken = localStorage.getItem('token');
-//         if(accessToken) {
-//             config.headers.Authorization = `Bearer ${accessToken}`;
-//             };
-//         return config;
-//     },
-//     (error)=>{
-//         return Promise.reject(error);
-//     }
-
-    
-// )
 
 export default axiosInstance;
