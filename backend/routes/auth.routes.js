67a77a76d9acc066eaa0e1c03c2a19bce3e848f8
RajatSharma <withrajatsharma@gmail.com>
2024-06-20 21:56:43 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,21 +1,20 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {signUp,login, getUser,logout} = require("../controllers/auth.controller.js");
-const {auth} = require("../middlewares/auth.js")
+const {
+  signUp,
+  login,
+  getUser,
+  logout,
+} = require("../controllers/auth.controller.js");
+const { auth } = require("../middlewares/auth.js");
 
-
-
-router.post("/create-account",signUp);
+router.post("/create-account", signUp);
 
 router.post("/login", login);
 
-router.get("/get-user",auth,getUser);
+router.get("/get-user", auth, getUser);
 
-router.get("/logout",auth,logout);
-
-
-
+router.get("/logout", auth, logout);
 
 module.exports = router;
