68fee6eda9513ac1a73a6c76ed75afff20bdb08d
RajatSharma <withrajatsharma@gmail.com>
2024-06-21 18:55:22 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -6,33 +6,38 @@ export const UserContext = createContext();
 const UserContextProvider = ({children}) => {
 
     const [userInfo, setUserInfo] = useState(null);
+    const [loading,setLoading] = useState(true);
 
     const getUserInfo = async () => {
+
         try {
           const response = await axiosInstance.get("/auth/get-user");
     
           if (response.data.user) {
-            
+            // setLoading(false);            
             setUserInfo(response.data.user);
-            
           }
         } catch (error) {
+          // setLoading(false);
           setUserInfo(null);
           if(!error.response.data.success){
             console.log(`error: ${error.response.data.message}`);
           }
         }
+        setLoading(false);
       };
 
 
       useEffect(()=>{
-        getUserInfo();
+
+          getUserInfo();
+
       },[])
 
 
 
     return (
-        <UserContext.Provider value={{userInfo, setUserInfo}} >
+        <UserContext.Provider value={{userInfo, setUserInfo,loading,setLoading}} >
             {children}
         </UserContext.Provider>
     )
