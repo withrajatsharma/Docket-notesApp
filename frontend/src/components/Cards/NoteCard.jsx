a51a91997108d71fee83b24dc133fb79fd996073
RajatSharma <withrajatsharma@gmail.com>
2024-06-23 21:49:53 +0530

    Update NoteCard.jsx

--- frontend/src/components/Cards/NoteCard.jsx
+++ frontend/src/components/Cards/NoteCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {MdOutlinePushPin , MdCreate, MdDelete} from "react-icons/md"
+import { FaHeart } from "react-icons/fa";
 import moment from 'moment';
 
 
@@ -12,6 +13,9 @@ const NoteCard = ({
     onEdit,
     onDelete,
     onPinNote,
+    isLiked,
+    onLike,
+    openLiked
 }) => {
   return (
     <div className='  border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out'>
@@ -20,7 +24,13 @@ const NoteCard = ({
                 <h6 className=' text-sm font-medium'>{title}</h6>
                 <span className=' text-xs text-slate-500'>{moment(date).format("Do MMM YYYY")}</span>
             </div>
-            <MdOutlinePushPin className={`icon-btn ${isPinned?"text-primary":"text-slate-300"}`} onClick={onPinNote}/>
+            <div className='flex flex-col gap-4'>
+                {
+                    openLiked&&<MdOutlinePushPin className={`icon-btn ${isPinned?"text-primary":"text-slate-300"}`} onClick={onPinNote}/>
+
+                }
+            <FaHeart className={`text-xl cursor-pointer hover:text-red-500 ${isLiked?"text-red-500":"text-slate-300"}`} onClick={onLike}/>
+            </div>
         </div>
         <p className=' text-xs text-slate-600 mt-2'>{content?.slice(0,60)}</p>
 
