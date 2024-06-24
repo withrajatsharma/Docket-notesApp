b544c77dc2c683e666eb8f86985fe21183a7a418
RajatSharma <withrajatsharma@gmail.com>
2024-06-25 02:19:46 +0530

    animation setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React, { Suspense, lazy, useContext, useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import OpenRoute from "./Routes/OpenRoute.jsx";
 import PrivateRoute from "./Routes/PrivateRoute.jsx";
 import Landing from "./pages/Landing/Landing.jsx";
 import { UserContext } from "./context/UserContext.jsx";
 // import Home from "./pages/Home/Home.jsx"
-import SignUp from "./pages/SignUp/SignUp.jsx"
-import Login from "./pages/Login/Login.jsx"
+import SignUp from "./pages/SignUp/SignUp.jsx";
+import Login from "./pages/Login/Login.jsx";
 import Loading from "./components/Loading/Loading.jsx";
-
+import { AnimatePresence } from "framer-motion";
 
 const Home = lazy(() => import("./pages/Home/Home.jsx"));
 // const Login = lazy(() => import("./pages/Login/Login.jsx"));
@@ -16,54 +21,70 @@ const Home = lazy(() => import("./pages/Home/Home.jsx"));
 // const Landing = lazy(() => import("./pages/Landing/Landing.jsx"));
 
 const App = () => {
+  const { userInfo, setUserInfo, loading, setLoading } =
+    useContext(UserContext);
+  const location = useLocation();
 
-  const {userInfo,setUserInfo,loading,setLoading} = useContext(UserContext);
-
-  
   return (
-      <Router>
-          <Routes>
-            <Route
-              path="/"
-              exact
-              element={
-                <OpenRoute userInfo={userInfo}>
-                 {  loading?<Loading/>:<Landing setUserInfo={setUserInfo} setLoading={setLoading} />}
-                </OpenRoute>
-              }
-            />
-            <Route
-              path="/dashboard"
-              exact
-              element={
-                <PrivateRoute userInfo={userInfo}>
-                  <Home userInfo={userInfo} setUserInfo={setUserInfo} loading={loading} setLoading={setLoading} />
-                </PrivateRoute>
-              }
-            />
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          exact
+          element={
+            <OpenRoute userInfo={userInfo}>
+              {loading ? (
+                <Loading />
+              ) : (
+                <Landing setUserInfo={setUserInfo} setLoading={setLoading} />
+              )}
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          exact
+          element={
+            <PrivateRoute userInfo={userInfo}>
+              <Home
+                userInfo={userInfo}
+                setUserInfo={setUserInfo}
+                loading={loading}
+                setLoading={setLoading}
+              />
+            </PrivateRoute>
+          }
+        />
 
-            <Route
-              path="/login"
-              exact
-              element={
-                <OpenRoute userInfo={userInfo}>
-                 { loading?<Loading/>:<Login setUserInfo={setUserInfo} setLoading={setLoading} />}
-                </OpenRoute>
-              }
-            />
+        <Route
+          path="/login"
+          exact
+          element={
+            <OpenRoute userInfo={userInfo}>
+              {loading ? (
+                <Loading />
+              ) : (
+                <Login setUserInfo={setUserInfo} setLoading={setLoading} />
+              )}
+            </OpenRoute>
+          }
+        />
 
-            <Route
-              path="/signup"
-              exact
-              element={
-                <OpenRoute userInfo={userInfo}>
-                  { loading?<Loading/>:<SignUp setUserInfo={setUserInfo} setLoading={setLoading} />}
-                </OpenRoute>
-              }
-            />
-          </Routes>
-
-      </Router>
+        <Route
+          path="/signup"
+          exact
+          element={
+            <OpenRoute userInfo={userInfo}>
+              {loading ? (
+                <Loading />
+              ) : (
+                <SignUp setUserInfo={setUserInfo} setLoading={setLoading} />
+              )}
+            </OpenRoute>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import UserContextProvider from './context/UserContext.jsx'
+import {BrowserRouter as Router} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserContextProvider>
+      <Router>
     <App />
+      </Router>
     </UserContextProvider>
   </React.StrictMode>,
 )
