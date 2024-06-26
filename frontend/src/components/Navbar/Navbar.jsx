5b0bfbae63eaa8a803c85c71bc8cd87c8ef9aef6
RajatSharma <withrajatsharma@gmail.com>
2024-06-26 22:04:55 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,19 @@
 import React, { useState } from "react";
-import ProfileInfo from "../Cards/ProfileInfo";
 import { Link, useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import axiosInstance from "../../utils/axiosInstance";
-import { FaHeart } from "react-icons/fa";
-import { FaNoteSticky } from "react-icons/fa6";
 import logo from "../../assets/reshot-icon-sticky-notes-MZFEWVHU7A.svg";
 
 const Navbar = ({
   userInfo,
-  setAllNotes,
   setUserInfo,
   onSearchNote,
   handleClearSearch,
   setLoading,
-  getLikedNotes,
-  getAllNotes,
-  openLiked,
 }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const navigate = useNavigate();
-
-  const onLogout = async () => {
-    setLoading(true);
-    try {
-      const response = await axiosInstance.get("/auth/logout");
-
-      if (response && response.data.success) {
-        setUserInfo(null);
-        setAllNotes("");
-        navigate("/", { replace: true });
-      }
-    } catch (error) {
-      console.log(`error: ${error}`);
-    }
-    setLoading(false);
-  };
 
   const handleSearch = () => {
     if (searchQuery) {
@@ -75,7 +52,19 @@ const Navbar = ({
     setLoading(false);
   };
 
-  return (
+  return userInfo ? (
+    <div className=" items-center flex  justify-center pt-5 ">
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      
+    </div>
+  ) : (
     <div className="bg-white flex items-center justify-between  px-5 sm:px-8  w-full py-2 ">
       <Link to={"/"}>
         <div className="-ml-2 sm:ml-0 flex items-center">
@@ -83,47 +72,12 @@ const Navbar = ({
           <p className=" -ml-1 sm:ml-0 sm:text-xl text-lg font-bold ">DOCKET</p>
         </div>
       </Link>
-
-      {userInfo ? (
-        <>
-          <SearchBar
-            value={searchQuery}
-            onChange={({ target }) => {
-              setSearchQuery(target.value);
-            }}
-            handleSearch={handleSearch}
-            onClearSearch={onClearSearch}
-          />
-
-          <button
-            className="flex  gap-2 items-center"
-            onClick={() => {
-              openLiked ? getLikedNotes() : getAllNotes();
-            }}
-          >
-            {openLiked ? (
-              <>
-                liked notes
-                <FaHeart className={`text-xl text-red-500`} />
-              </>
-            ) : (
-              <>
-                all notes
-                <FaNoteSticky className={`text-xl text-primary`} />
-              </>
-            )}
-          </button>
-
-          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
-        </>
-      ) : (
-        <button
-          onClick={handleTryDemo}
-          className="btn-primary px-5 rounded-lg w-fit"
-        >
-          Try Demo
-        </button>
-      )}
+      <button
+        onClick={handleTryDemo}
+        className="btn-primary px-5 rounded-lg w-fit"
+      >
+        Try Demo
+      </button>
     </div>
   );
 };
