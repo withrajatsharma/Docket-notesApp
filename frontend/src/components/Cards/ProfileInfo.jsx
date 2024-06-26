917f517b43a7ba4f1ec1154fa91ca07dde523f3c
RajatSharma <withrajatsharma@gmail.com>
2024-06-26 22:04:49 +0530

    Update ProfileInfo.jsx

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { getInitials } from '../../utils/helper'
 
-const ProfileInfo = ({onLogout,userInfo}) => {
+const ProfileInfo = ({userInfo}) => {
   return (
-    <div className='flex items-center gap-3'>
-            <div className=' w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>{
+
+    <div>
+    <div className='flex items-center gap-3 ml-5'>
+            <div className=' w-12 h-12 flex items-center justify-center rounded-full text-lg font-medium bg-secondary'>{
                 getInitials(userInfo?.fullName)
                 }</div>
             <div>
-                <p className=' text-sm font-medium '>{userInfo?.fullName}</p>
-                <button className=' text-sm text-slate-700 underline ' onClick={onLogout}>
-                    Logout
-                </button>
+                <p className=' text-base font-medium '>{userInfo?.fullName}</p>
+               
+         
             </div>
+    </div>
     </div>
   )
 }
