a3871d228749b3c654fb4be250c3c2ae2cded7a5
RajatSharma <withrajatsharma@gmail.com>
2024-06-27 23:08:16 +0530

    Update NoteCard.jsx

--- frontend/src/components/Cards/NoteCard.jsx
+++ frontend/src/components/Cards/NoteCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {MdOutlinePushPin , MdCreate, MdDelete} from "react-icons/md"
-import { FaHeart } from "react-icons/fa";
+import { FaRegHeart,FaHeart,FaStar } from "react-icons/fa";
 import moment from 'moment';
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 
 const NoteCard = ({
@@ -18,33 +18,67 @@ const NoteCard = ({
     openLiked
 }) => {
   return (
-    <div className='  border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out'>
-        <div className=' flex items-center justify-between'>
+    <div
+       onClick={onEdit}
+     className='h-52 w-36 sm:w-auto  rounded-xl p-4 bg-[#c4c4c43a] hover:shadow-2xl transition-all ease-in-out flex flex-col justify-between'>
+       
+       <div className='overflow-hidden'>
+
+      
+        <div className=' flex items-center justify-between '>
             <div>
-                <h6 className=' text-sm font-medium'>{title}</h6>
-                <span className=' text-xs text-slate-500'>{moment(date).format("Do MMM YYYY")}</span>
+                <h6 className=' text-base font-medium'>{title}</h6>
             </div>
-            <div className='flex flex-col gap-4'>
+            <div className='mr-1 '>
                 {
-                    openLiked&&<MdOutlinePushPin className={`icon-btn ${isPinned?"text-primary":"text-slate-300"}`} onClick={onPinNote}/>
+                    openLiked&&(isPinned?<FaStar className={`icon-btn text-yellow-400 hover:text-yellow-500 hover:scale-110 transition-all ease`} onClick={(e)=>{
+                            e.stopPropagation();
+                        onPinNote();}}/>:<FaRegStar className={`icon-btn hover:scale-110 transition-all ease hover:text-slate-400`} onClick={(e)=>{
+                            e.stopPropagation();
+                        onPinNote();}}/>)
 
                 }
-            <FaHeart className={`text-xl cursor-pointer hover:text-red-500 ${isLiked?"text-red-500":"text-slate-300"}`} onClick={onLike}/>
+            
             </div>
         </div>
-        <p className=' text-xs text-slate-600 mt-2'>{content?.slice(0,60)}</p>
 
+        <p className=' text-xs text-slate-600 mt-2'>{content?.slice(0,180)}</p>
+
+        </div>
+
+
+        <div>
+
+        
             <div className=' flex items-center justify-between gap-2'>
                 <div className=' text-xs text-slate-500'>
-    {tags.map((item)=>`#${item} `)}
+    {tags.map((item,index)=>index<5&&`#${item} `)}
                 </div>
-                <div className='flex items-center gap-2'>
-                    <MdCreate className=' icon-btn hover:text-green-600'
-                    onClick={onEdit}/>
-                    <MdDelete className=' icon-btn hover:text-red-500'
-                    onClick={onDelete}/>
+               
+            </div>
+
+            <div className=' flex justify-between items-center mt-4'>
+            <span className=' text-sm text-slate-700'>{moment(date).format("MMM DD, YYYY")}</span>
+            <div className='flex items-center gap-4 mr-1'>
+                    <RiDeleteBin6Line className='transition-all ease icon-btn hover:scale-110 hover:text-red-600'
+                    onClick={(e)=>{
+                        e.stopPropagation();
+                        onDelete();
+                    }}/>
+                    {
+                        isLiked?<FaHeart className={`icon-btn transition-all ease  hover:scale-110 hover:text-red-500 text-red-400 `} onClick={(e)=>{
+                            e.stopPropagation();
+                            onLike();
+                        }}/>:<FaRegHeart className={`icon-btn  hover:text-slate-400 transition-all ease hover:scale-110 `} onClick={(e)=>{
+                            e.stopPropagation();
+                            onLike();
+                        }}/>
+                    }
+                    
+                    
                 </div>
             </div>
+        </div>
 
     </div>
   )
