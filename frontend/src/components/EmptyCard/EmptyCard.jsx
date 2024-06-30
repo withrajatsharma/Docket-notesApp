68b5b76fbb673b4275d8b052ce65bbedf263ea22
RajatSharma <withrajatsharma@gmail.com>
2024-06-30 16:16:38 +0530

    Update EmptyCard.jsx

--- frontend/src/components/EmptyCard/EmptyCard.jsx
+++ frontend/src/components/EmptyCard/EmptyCard.jsx
@@ -3,11 +3,11 @@ import Loading from '../Loading/Loading'
 
 const EmptyCard = ({imgSrc,message,loading}) => {
   return (
-   loading?<Loading />: <div className=' flex flex-col items-center justify-center mt-32'>
+   loading?<Loading />: <div className=' flex flex-col items-center justify-center'>
 
-      <img src={imgSrc} alt="" className=' w-60' />
+      <img src={imgSrc} alt="" className=' opacity-75 w-60' />
 
-        <p className=' w-[40%] text-sm font-medium text-slate-700 text-center leading-7 mt-5'>
+        <p className=' px-80 text-sm font-medium text-slate-700 text-center leading-7 mt-5'>
           {message}
         </p>
     </div>
